320559fa27a32dab204c4c3c28094167c82dd074
Yuzhong Huang <yuzhongh@usc.edu>
2022-10-07 13:35:26 -0700

    add switch server

--- src/extension.ts
+++ src/extension.ts
@@ -18,6 +18,13 @@ export function activate(context: vscode.ExtensionContext) {
 	statusBarItem.show();
 	context.subscriptions.push(statusBarItem);
 
+	const HOSTS = ["yuzhongh@discovery1.usc.edu", "yuzhongh@discovery2.usc.edu"];
+	const SRC = "c:\\Users\\yuzho\\surface_recon\\";
+	const TGT = "/home1/yuzhongh/surface_recon/";
+	let index = 0;
+	let last_time = Date.now();
+	let enable = true;
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -27,17 +34,30 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage(lastMessage);
 	}));
 
-	const HOST = "yuzhongh@discovery1.usc.edu";
-	const SRC = "c:\\Users\\yuzho\\surface_recon\\";
-	const TGT = "/home1/yuzhongh/surface_recon/";
+	context.subscriptions.push(vscode.commands.registerCommand('savescp.enable', () => {
+		enable = true;
+		vscode.window.showInformationMessage("SaveSCP Enabled");
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('savescp.disable', () => {
+		enable = false;
+		vscode.window.showInformationMessage("SaveSCP Disabled");
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('savescp.switch', () => {
+		index = (index + 1) % 2;
+		vscode.window.showInformationMessage("Switch to server: " + HOSTS[index]);
+	}));
 
 	vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
-		if (document.uri.scheme === "file") {
+		if (enable && (Date.now() - last_time > 1000) && document.uri.scheme === "file") {
+			last_time = Date.now()
+
 			let filename = document.fileName;
 			if (filename.includes(".pyc") || filename.includes(".git") || !filename.includes(SRC)) {
 				return;
 			}
-			let cmd = `scp ${filename} ${HOST}:${filename.replace(SRC, TGT)}`;
+			let cmd = `scp ${filename} ${HOSTS[index]}:${filename.replace(SRC, TGT)}`;
 			statusBarItem.text = cmd;
 
 			lastMessage = cmd;
