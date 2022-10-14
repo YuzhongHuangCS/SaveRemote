bdf47e3b8f2b13fa6fd6d618d361acd923eb92b8
Yuzhong Huang <yuzhongh@usc.edu>
2022-10-14 12:13:51 -0700

    add non-scp method

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs'
 import * as child_process from 'child_process'
+import * as FormData from 'form-data';
+const axios = require('axios');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -22,8 +25,9 @@ export function activate(context: vscode.ExtensionContext) {
 	const SRC = "c:\\Users\\yuzho\\surface_recon\\";
 	const TGT = "/home1/yuzhongh/surface_recon/";
 	let index = 0;
-	let last_time = Date.now();
+	let lastTime = Date.now();
 	let enable = true;
+	let useSCP = false;
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -49,44 +53,72 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage("Switch to server: " + HOSTS[index]);
 	}));
 
+	context.subscriptions.push(vscode.commands.registerCommand('savescp.switchSaveMethod', () => {
+		useSCP = !useSCP;
+		vscode.window.showInformationMessage(`Switch to useSCP: ${useSCP}`);
+	}));
+
 	vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
-		if (enable && (Date.now() - last_time > 1000) && document.uri.scheme === "file") {
-			last_time = Date.now()
+		if (enable && (Date.now() - lastTime > 1000) && document.uri.scheme === "file") {
+			lastTime = Date.now()
 
 			let filename = document.fileName;
 			if (filename.includes(".pyc") || filename.includes(".git") || !filename.includes(SRC)) {
 				return;
 			}
-			let cmd = `scp ${filename} ${HOSTS[index]}:${filename.replace(SRC, TGT)}`;
-			statusBarItem.text = cmd;
 
-			lastMessage = cmd;
-			let scp = child_process.spawn(cmd, {shell: true});
-			scp.stdout.on('data', (data) => {
-				lastMessage += data.toString();
-				vscode.window.showInformationMessage(`stdout: ${data}`);
-			});
-			  
-			scp.stderr.on('data', (data) => {
-				lastMessage += data.toString();
-				vscode.window.showInformationMessage(`stderr: ${data}`);
-			});
-			  
-			scp.on('close', (code) => {
-				if (code === 0) {
+			if (useSCP) {
+				let cmd = `scp ${filename} ${HOSTS[index]}:${filename.replace(SRC, TGT)}`;
+				statusBarItem.text = cmd;
+				lastMessage = cmd;
+
+				let scp = child_process.spawn(cmd, {shell: true});
+				scp.stdout.on('data', (data) => {
+					lastMessage += data.toString();
+					vscode.window.showInformationMessage(`stdout: ${data}`);
+				});
+
+				scp.stderr.on('data', (data) => {
+					lastMessage += data.toString();
+					vscode.window.showInformationMessage(`stderr: ${data}`);
+				});
+
+				scp.on('close', (code) => {
+					if (code === 0) {
+						statusBarItem.text = "Saved";
+					} else {
+						vscode.window.showInformationMessage(`scp process exited with code ${code}`);
+						statusBarItem.text = "Failed";
+					}
+				})
+
+				setTimeout(() => {
+					if (scp.connected) {
+						vscode.window.showInformationMessage(`scp process timeout after 5 sec`);
+						scp.kill();
+					}
+				}, 5000)
+			} else {
+				let workspacePath = vscode.workspace.getWorkspaceFolder(document.uri)!.uri.path;
+				let relativePath = document.uri.path.substring(workspacePath.length);
+
+				let cmd = `Sending ${relativePath}`;
+				statusBarItem.text = cmd;
+				lastMessage = cmd;
+
+				let formData = new FormData();
+				formData.append("auth", "nOgjXyCG68tq2E8");
+				formData.append("path", relativePath);
+				formData.append('file', fs.createReadStream(filename));
+				axios.post('http://localhost:8000/', formData).then((response:any) => {
 					statusBarItem.text = "Saved";
-				} else {
-					vscode.window.showInformationMessage(`scp process exited with code ${code}`);
+					lastMessage += "; " + response.data;
+				}).catch((error:any) => {
+					vscode.window.showInformationMessage(error.message);
 					statusBarItem.text = "Failed";
-				}
-			})
-
-			setTimeout(() => {
-				if (scp.connected) {
-					vscode.window.showInformationMessage(`scp process timeout after 5 sec`);
-					scp.kill();
-				}
-			}, 5000)
+					lastMessage = error.message
+				});
+			}
 		}
 	});
 }
