d58b8084414a80072f9238a63cdf460571392112
Yuzhong Huang <yuzhongh@usc.edu>
2023-04-08 23:03:03 -0700

    rename to SaveRemote

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# savescp README
+# SaveRemote README
 
-The HPC cluster have stringent requirement on CPU & memory usage on login node, so I couldn't run a long-running process like vscode remote server. Therefore, I write this extension as a minimized replacement to run a scp command everytime a file is saved.
+The HPC cluster have stringent requirement on CPU & memory usage on login node, so I couldn't use vscode remote server as it consumes a lot of resources.
+This extension is a minimized replacement of vscode remote that sync your file to the remote server everytime it is saved.
--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs'
-import * as child_process from 'child_process'
 import * as FormData from 'form-data';
 const axios = require('axios');
 
@@ -12,50 +11,37 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "savescp" is now active!');
+	console.log('Congratulations, your extension "saveremote" is now active!');
+	//console.log(JSON.stringify(vscode.workspace.getConfiguration("saveremote")));
 
 	let lastMessage = '';
 	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
 	statusBarItem.text = "Ready";
-	statusBarItem.command = 'savescp.lastMessage';
+	statusBarItem.command = 'saveremote.lastMessage';
 	statusBarItem.show();
 	context.subscriptions.push(statusBarItem);
 
-	const HOSTS = ["yuzhongh@discovery1.usc.edu", "yuzhongh@discovery2.usc.edu"];
 	const SRC = "c:\\Users\\yuzho\\surface_recon\\";
-	const TGT = "/home1/yuzhongh/surface_recon/";
-	let index = 0;
 	let lastTime = Date.now();
 	let enable = true;
-	let useSCP = false;
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	context.subscriptions.push(vscode.commands.registerCommand('savescp.lastMessage', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('saveremote.lastMessage', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage(lastMessage);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('savescp.enable', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('saveremote.enable', () => {
 		enable = true;
-		vscode.window.showInformationMessage("SaveSCP Enabled");
+		vscode.window.showInformationMessage("saveremote Enabled");
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('savescp.disable', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('saveremote.disable', () => {
 		enable = false;
-		vscode.window.showInformationMessage("SaveSCP Disabled");
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('savescp.switch', () => {
-		index = (index + 1) % 2;
-		vscode.window.showInformationMessage("Switch to server: " + HOSTS[index]);
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('savescp.switchSaveMethod', () => {
-		useSCP = !useSCP;
-		vscode.window.showInformationMessage(`Switch to useSCP: ${useSCP}`);
+		vscode.window.showInformationMessage("saveremote Disabled");
 	}));
 
 	vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
@@ -67,58 +53,26 @@ export function activate(context: vscode.ExtensionContext) {
 				return;
 			}
 
-			if (useSCP) {
-				let cmd = `scp ${filename} ${HOSTS[index]}:${filename.replace(SRC, TGT)}`;
-				statusBarItem.text = cmd;
-				lastMessage = cmd;
+			let workspacePath = vscode.workspace.getWorkspaceFolder(document.uri)!.uri.path;
+			let relativePath = document.uri.path.substring(workspacePath.length);
 
-				let scp = child_process.spawn(cmd, {shell: true});
-				scp.stdout.on('data', (data) => {
-					lastMessage += data.toString();
-					vscode.window.showInformationMessage(`stdout: ${data}`);
-				});
+			let cmd = `Sending ${relativePath}`;
+			statusBarItem.text = cmd;
+			lastMessage = cmd;
 
-				scp.stderr.on('data', (data) => {
-					lastMessage += data.toString();
-					vscode.window.showInformationMessage(`stderr: ${data}`);
-				});
+			let formData = new FormData();
+			formData.append("auth", "nOgjXyCG68tq2E8");
+			formData.append("path", relativePath);
+			formData.append('file', fs.createReadStream(filename));
+			axios.post('http://localhost:8765/', formData).then((response:any) => {
+				statusBarItem.text = "Saved";
+				lastMessage += "; " + response.data;
+			}).catch((error:any) => {
+				vscode.window.showInformationMessage(error.message);
+				statusBarItem.text = "Failed";
+				lastMessage = error.message
+			});
 
-				scp.on('close', (code) => {
-					if (code === 0) {
-						statusBarItem.text = "Saved";
-					} else {
-						vscode.window.showInformationMessage(`scp process exited with code ${code}`);
-						statusBarItem.text = "Failed";
-					}
-				})
-
-				setTimeout(() => {
-					if (scp.connected) {
-						vscode.window.showInformationMessage(`scp process timeout after 5 sec`);
-						scp.kill();
-					}
-				}, 5000)
-			} else {
-				let workspacePath = vscode.workspace.getWorkspaceFolder(document.uri)!.uri.path;
-				let relativePath = document.uri.path.substring(workspacePath.length);
-
-				let cmd = `Sending ${relativePath}`;
-				statusBarItem.text = cmd;
-				lastMessage = cmd;
-
-				let formData = new FormData();
-				formData.append("auth", "nOgjXyCG68tq2E8");
-				formData.append("path", relativePath);
-				formData.append('file', fs.createReadStream(filename));
-				axios.post('http://localhost:8765/', formData).then((response:any) => {
-					statusBarItem.text = "Saved";
-					lastMessage += "; " + response.data;
-				}).catch((error:any) => {
-					vscode.window.showInformationMessage(error.message);
-					statusBarItem.text = "Failed";
-					lastMessage = error.message
-				});
-			}
 		}
 	});
 }
