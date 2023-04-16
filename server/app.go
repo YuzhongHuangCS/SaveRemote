68e533d78acbc91a71e7c4f1bdbd36a3ca13b14f
Yuzhong Huang <yuzhongh@usc.edu>
2023-04-16 14:14:31 -0700

    add download to go version

--- server/app.go
+++ server/app.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"os"
 	"io"
+	"io/ioutil"
 	"path"
 	"runtime"
 	"net/http"
+	"encoding/json"
 )
 
 // 10 MB
@@ -14,8 +16,8 @@ const MAX_UPLOAD_SIZE = 10 * 1024 * 1024
 var addr = os.Getenv("ADDR")
 var auth = os.Getenv("AUTH")
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	if (r.Method == "POST") {
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if (r.Method == http.MethodPost) {
 		r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
 		if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
 			fmt.Println(err)
@@ -59,12 +61,67 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	if (r.Method == http.MethodPost) {
+		r.ParseForm()
+
+		if len(r.Form["auth"]) > 0 && r.Form["auth"][0] == auth && len(r.Form["path"]) > 0 {
+			readPath := r.Form["path"][0]
+
+			fi, err := os.Stat(readPath)
+			if err != nil {
+				fmt.Println("NotFound:", readPath)
+				http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+				return
+			}
+
+			switch mode := fi.Mode(); {
+				case mode.IsRegular():
+					fileBytes, err := ioutil.ReadFile(readPath)
+					if err != nil {
+						fmt.Println("NotFound:", readPath)
+						http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+						return
+					}
+					w.Header().Set("content-type", "application/octet-stream")
+					w.Write(fileBytes)
+					fmt.Println("Downloaded:", readPath)
+				case mode.IsDir():
+					files, err := ioutil.ReadDir(readPath)
+					if err != nil {
+						fmt.Println("NotFound:", readPath)
+						http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+						return
+					}
+
+					fullPaths := []string {}
+					for _, file := range files {
+						fullPaths = append(fullPaths, path.Join(readPath, file.Name()));
+					}
+
+					w.Header().Set("content-type", "application/json")
+					json.NewEncoder(w).Encode(map[string][]string {"files": fullPaths})
+					fmt.Println("Listed:", readPath)
+				default:
+					fmt.Println("NotFound:", readPath)
+					http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			}
+		} else {
+			fmt.Println(r.Form)
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		}
+	} else {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	runtime.GOMAXPROCS(1)
 	if len(addr) == 0 {addr = ":8765"}
 
 	fmt.Printf("Starting server with addr=%s, auth=%s\n", addr, auth)
 	fmt.Println("Edit environment variable ADDR and AUTH to customize")
-	http.HandleFunc("/", requestHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/download", downloadHandler)
 	http.ListenAndServe(addr, nil)
 }
